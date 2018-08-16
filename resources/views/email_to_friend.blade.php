a582b0044aac5fcb1c15bae8b77ecbe82f3b20c2
yjren <602598484@qq.com>
2018-08-16 23:43:46 +0800

    up

--- resources/views/email_to_friend.blade.php
+++ resources/views/email_to_friend.blade.php
@@ -1,5 +1,5 @@
 Hi Dear,<br>
-Your friend {{$uname}} has invited you to register on this amazing website: <a href="www.escortpie.com,">www.escortpie.com</a>,it is the most visited and popular advertising website. Register Today, we will walk you through step by step to post your first stunning ad and attract your most desired audiences. And you will be successful and content with the busy traffic of our website.
+Your friend {{$uname}} has invited you to register on this amazing website: <a href="http://www.escortpie.com">www.escortpie.com</a>,it is the most visited and popular advertising website. Register Today, we will walk you through step by step to post your first stunning ad and attract your most desired audiences. And you will be successful and content with the busy traffic of our website.
 <br><br>
 
 Sincerely yours,
